d0a9a1c628460ac7a9d22b24e2ebd9324e3442d8
AdrienHdz <adrien.hernandez@hec.ca>
2023-05-29 19:34:43 -0400

    chore: Added config file for hyperparameter config

---
+++ parallelize_torch/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+
+class MainConfig(BaseSettings):
+    # hyperparameters
+    num_workers: int = 4
+    use_gpu: bool = True
+    smoke_test: bool = False
+
+    # smoke test params
+    num_cpus_smoke_test: int = 3
+
+    class Config:
+        env_prefix = "TRAINING_"
