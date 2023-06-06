dc5f047c718c312e0f409442f08a05c26c4d721a
AdrienHdz <adrien.hernandez@hec.ca>
2023-06-06 17:27:59 -0400

    style: Modified style of code with black

--- submit_training.py
+++ submit_training.py
@@ -3,16 +3,12 @@ from ray.job_submission import JobSubmissionClient
 
 client = JobSubmissionClient("http://127.0.0.1:8265")
 
-kick_off_pytorch_benchmark = (
-    "python main.py"
-)
+kick_off_pytorch_benchmark = "python main.py"
 
 
 submission_id = client.submit_job(
     entrypoint=kick_off_pytorch_benchmark,
-    runtime_env={
-        "working_dir": "./parallelize_torch/"
-    }
+    runtime_env={"working_dir": "./parallelize_torch/"},
 )
 
 print("Use the following command to follow this Job's logs:")
